89be0fd82efcc716a00e2bce09860a94f2b43025
Ndarti <lizaxxxxxxxxxx@gmail.com>
2025-06-14 05:18:55 +0300

    update bots

--- new bot_tg_api.py
+++ new bot_tg_api.py
@@ -4,36 +4,34 @@ import time
 import requests
 token='7817172796:AAEpCDT_1nXbhSm9Uy_FJ8QlOmSx-eUq2W8'
 URL= f'https://api.telegram.org/bot{token}/'
-#https://api.telegram.org/bot7817172796:AAEpCDT_1nXbhSm9Uy_FJ8QlOmSx-eUq2W8/getUpdates
+
 def send(id, text):
-    params = [{"id": id, "text": text}]
-    print(params)
-    response = requests.post(URL + "sendMessage", params=params)
-    #URL = f'https://api.telegram.org/bot7817172796:AAEpCDT_1nXbhSm9Uy_FJ8QlOmSx-eUq2W8/sendMessage'
-    return response.json()
+    params = {
+        "chat_id": id,
+        "text":text
+    }
+    res = requests.get(URL + "sendMessage", params=params)
+    res.raise_for_status()
+    return res.json()
 def get(offset):#take text with requests
     params = {"offset": offset, "timeout": 30} if offset else {"timeout": 30}
     response = requests.get(URL + "getUpdates", params=params)
     return response.json().get("result", [])
 
-async def main ():
+def main ():
     off = None
     while True:
         updates = get(off)
-
         for update in updates:
             offset = update["update_id"] + 1
             chat_id = update["message"]["chat"]["id"]
             text = update["message"]["text"]
-            print(offset)
-            print(chat_id)
             print(send(chat_id, "Привет"))
+            if text == "/start":
 
-        if text == "start":
-
-            send(chat_id, "Привет")
-        else:
-            send(chat_id, f"Ты написал: {text}")
+                send(chat_id, "Привет")
+            else:
+                send(chat_id, f"Ты написал: {text}")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
